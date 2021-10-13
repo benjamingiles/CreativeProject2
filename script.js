d88606b73c2a58a04c4d9fd4a8673452e3d99f4d
ki11errabbit <unlikelytitan@gmail.com>
2021-10-13 09:48:44 -0600

    Added all of the URLs for each ot the APIs we are using

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-stuff
+
+
+
+
+const AxolotlURL = "https://axoltlapi.herokuapp.com/";//random each time?
+//const BearURL = "https://placebear.com/"; //height/width of pixels
+const CatURL = "https://aws.random.cat/meow";//Random each time;
+const DogURL = "https://random.dog/woof.json"; //Random each time;
+const DuckURL = "https://random-d.uk/api/v2/random";//Random each time
+const FoxURL = "https://randomfox.ca/floof/";//Random each time
+const ShibaURL = "http://shibe.online/api/shibes?count=";//[1-100]&urls=[true/false]&httpsUrls=[true/false]
