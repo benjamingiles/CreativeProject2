a56bea289ece1846a6a1c9942d45c34262488096
ki11errabbit <unlikelytitan@gmail.com>
2021-10-14 19:33:44 -0600

    fixed errors

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const AxolotlURL = "https://axoltlapi.herokuapp.com/";//random each time?
+//const AxolotlURL = "https://axoltlapi.herokuapp.com/";//random each time?
 //const BearURL = "https://placebear.com/"; //height/width of pixels
 const CatURL = "https://aws.random.cat/meow";//Random each time;
 const DogURL = "https://random.dog/woof.json"; //Random each time;
-const DuckURL = "https://random-d.uk/api/v2/random";//Random each time
+//const DuckURL = "https://random-d.uk/api/v2/random";//Random each time
 const FoxURL = "https://randomfox.ca/floof/";//Random each time
-const ShibaURL = "http://shibe.online/api/shibes?count=";//[1-100]&urls=[true/false]&httpsUrls=[true/false]
+//const ShibaURL = "http://shibe.online/api/shibes?count=";//[1-100]&urls=[true/false]&httpsUrls=[true/false]
 
-
-function animalChooser() {
-  return Math.random() * NumberOfAnimals;
-}
 
 const NumberOfAnimals = 3;
 const SelectedAnimal = animalChooser();// 0 = cats, 1 = dogs, 2 = foxes
@@ -17,10 +13,21 @@ const SelectedAnimal = animalChooser();// 0 = cats, 1 = dogs, 2 = foxes
 let mainAnimal = 0; // 0 = cats, 1 = dogs, 2 = foxes
 let animalArray = [];
 
-function addCat() {
+function animalChooser() {
+  return Math.floor(Math.random() * NumberOfAnimals);
+}
+
+async function addCat() {
   let imageLink = "";
 
-  fetch(CatURL)
+  let response = await fetch(CatURL);
+  console.log(response);
+  let json = await response.json();
+  console.log(json);
+  imageLink = json.file;
+  console.log(imageLink);
+
+  await fetch(CatURL)
     .then(function(response) {
       console.log(response);
       return response.json();
@@ -33,10 +40,10 @@ function addCat() {
     return cat = {name:"cat", image:imageLink, cat:true, dog:false, fox:false};
 }
 
-function addDog() {
+async function addDog() {
   let imageLink = "";
 
-  fetch(DogURL)
+  await fetch(DogURL)
     .then(function(response) {
       console.log(response);
       return response.json();
@@ -49,10 +56,10 @@ function addDog() {
     return dog = {name:"dog", image:imageLink, cat:false, dog: true, fox:false};
 }
 
-function addFox() {
+async function addFox() {
   let imageLink = "";
 
-  fetch(FoxURL)
+  await fetch(FoxURL)
     .then(function(response) {
       console.log(response);
       return response.json();
@@ -66,40 +73,36 @@ function addFox() {
 }
 
 
-function addAnimal(whichAnimal) {
+async function addAnimal(whichAnimal) {
     if (whichAnimal === 0) {
-       animalArray.push(addCat());
+       animalArray.push(await addCat());
     }
     else if (whichAnimal === 1) {
-       animalArray.push(addDog());
+       animalArray.push(await addDog());
     }
     else if (whichAnimal === 2) {
-       animalArray.push(addFox());
+       animalArray.push(await addFox());
     }
 }
 
-function setMainAnimal() {
-  mainAnimal = animalChooser();
-}
 
-
-function buildArray() {
+async function buildArray() {
   let totalAnimals = 1;
-  setMainAnimal();
-  addAnimal(mainAnimal);
+  await addAnimal(SelectedAnimal);
 
   while (totalAnimals != 10) {
-    addAnimal(animalChooser());
+    await addAnimal(animalChooser());
     totalAnimals++;
   }
+  let results = "";
+  for (let i = 0; i < 9; i++) {
+    results += '<p>' + animalArray[i].name + '</p>';
+    results += '<img src="' + animalArray[i].image + '"/>';
+  }
+  document.getElementById("image").innerHTML = results;
 }
+buildArray();
 
-let results = "";
-for (let i = 0; i < 9; i++) {
-  results += '<p>' + animalArray[i].name + '</p>';
-  results += '<img src="' + animalArray[i].image + '"/>';
-}
-document.getElementById("image").innerHTML = results;
 
 
 function scrambleArray() {
